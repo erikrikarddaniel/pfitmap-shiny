a994cc490e02f31b40b5916a2ab57e4d55d30ef1
Daniel Lundin <erik.rikard.daniel@gmail.com>
2018-02-02 15:31:52 +0100

    Bumped version to 1.0.2

--- hmmer-import/R/hmmsearch2classification.r
+++ hmmer-import/R/hmmsearch2classification.r
@@ -9,7 +9,7 @@ suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(readr))
 suppressPackageStartupMessages(library(tidyr))
 
-SCRIPT_VERSION = "1.0.1"
+SCRIPT_VERSION = "1.0.2"
 
 # Get arguments
 option_list = list(
