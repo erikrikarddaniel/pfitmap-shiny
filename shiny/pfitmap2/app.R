006ee38f7628dea4f58432e06c42227e796fad63
Daniel Lundin <erik.rikard.daniel@gmail.com>
2018-02-28 20:40:46 +0100

    Selection of protein hierarchy elements.

--- shiny/pfitmap2/app.R
+++ shiny/pfitmap2/app.R
@@ -19,11 +19,20 @@ dbpool <- dbPool(
   drv = RSQLite::SQLite(),
   dbname = Sys.getenv('PFITMAP2_SQLITE')
 )
+
 onStop(function() {
-  poolClose(pool)
+  poolClose(dbpool)
 })
 
-dbsources <- dbpool %>% tbl('dbsources')
+# Setting up variables to connect to some tables
+accessions       <- dbpool %>% tbl('accessions')
+dbsources        <- dbpool %>% tbl('dbsources')
+hmm_profiles     <- dbpool %>% tbl('hmm_profiles')
+dupfree_proteins <- dbpool %>% tbl('dupfree_proteins')
+
+# Defining short cut data
+dbs            <- accessions %>% distinct(db) %>% filter(!is.na(db)) %>% arrange(db) %>% pull(db)
+psuperfamilies <- hmm_profiles %>% distinct(psuperfamily) %>% filter(!is.na(psuperfamily)) %>% pull(psuperfamily)
 
 # Define UI for application 
 ui <- fluidPage(
@@ -31,6 +40,37 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
+      selectInput(
+        'db', 'Database',
+        dbs, selected = 'refseq'
+      ),
+      selectInput(
+        'taxonrank', 'Taxon rank', 
+        list(
+          'Domain'  = 'tdomain',  'Phylum'  = 'tphylum', 'Class'   = 'tclass',
+          'Order'   = 'torder',   'Family'  = 'tfamily', 'Genus'   = 'tgenus',
+          'Species' = 'tspecies', 'Strain'  = 'tstrain'
+        )
+      ),
+      selectInput(
+        'proteinrank', 'Protein rank',
+        list(
+          'Superfamily' = 'psuperfamily', 'Family'   = 'pfamily',
+          'Class'       = 'pclass',       'Subclass' = 'psubclass',
+          'Group'       = 'pgroup'
+        ),
+        selected = c('pclass')
+      ),
+      wellPanel(
+        selectInput(
+          'psuperfamilies', 'Protein superfamilies',
+          c('', psuperfamilies), selected = c('NrdGRE'), 
+          multiple=T
+        ),
+        uiOutput('pfamilies'),
+        uiOutput('pclasses'),
+        uiOutput('psubclasses')
+      )
     ),
     mainPanel(
       htmlOutput('ssversion')
@@ -47,6 +87,48 @@ server <- function(input, output) {
       dbsources %>% pull(name),
       dbsources %>% pull(version),
       UI_VERSION
+    )
+  })
+
+  output$pfamilies = renderUI({
+    if ( length(input$psuperfamilies) > 0 ) {
+      pf <- hmm_profiles %>% filter(psuperfamily %in% input$psuperfamilies) %>%
+        distinct(pfamily) %>% pull(pfamily)
+    } else {
+      pf <- hmm_profiles %>% distinct(pfamily) %>% pull(pfamily)
+    }
+    
+    selectInput(
+      'pfamilies', 'Families',
+      pf, multiple = T
+    )
+  })
+
+  output$pclasses = renderUI({
+    if ( length(input$pfamilies) > 0 ) {
+      pc <- hmm_profiles %>% filter(pfamily %in% input$pfamilies) %>%
+        distinct(pclass) %>% pull(pclass)
+    } else {
+      pc <- hmm_profiles %>% distinct(pclass) %>% pull(pclass)
+    }
+    
+    selectInput(
+      'pclasses', 'Classes',
+      pc, multiple = T
+    )
+  })
+
+  output$psubclasses = renderUI({
+    if ( length(input$pclasses) > 0 ) {
+      pc <- hmm_profiles %>% filter(pclass %in% input$pclasses) %>%
+        distinct(psubclass) %>% pull(psubclass)
+    } else {
+      pc <- hmm_profiles %>% distinct(psubclass) %>% pull(psubclass)
+    }
+    
+    selectInput(
+      'psubclasses', 'Subclasses',
+      pc, multiple = T
     )
   })
 }
