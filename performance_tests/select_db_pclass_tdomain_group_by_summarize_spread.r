6818548a662eae099c7bab1f5a3ad363dfc4e491
Daniel Lundin <erik.rikard.daniel@gmail.com>
2018-03-07 13:16:00 +0100

    Filtering early appears to be faster, not driving with small table

--- performance_tests/select_db_pclass_tdomain_group_by_summarize_spread.r
+++ performance_tests/select_db_pclass_tdomain_group_by_summarize_spread.r
@@ -9,25 +9,23 @@
 #
 # Author: daniel.lundin@dbb.su.se
 
-suppressPackageStartupMessages(library(profvis))
-suppressPackageStartupMessages(library(pool))
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(dbplyr))
 suppressPackageStartupMessages(library(tidyr))
 
-dbpool <- dbPool(
+con <- DBI::dbConnect(
   drv    = RSQLite::SQLite(),
   dbname = 'hmmsearch2classification.sqlite3'
 )
 
 # Setting up variables to connect to some tables
-accessions           <- dbpool %>% tbl('accessions')
+accessions           <- con %>% tbl('accessions')
 accessions_mem       <- accessions %>% collect()
-hmm_profiles         <- dbpool %>% tbl('hmm_profiles')
+hmm_profiles         <- con %>% tbl('hmm_profiles')
 hmm_profiles_mem     <- hmm_profiles %>% collect()
-dupfree_proteins     <- dbpool %>% tbl('dupfree_proteins')
+dupfree_proteins     <- con %>% tbl('dupfree_proteins')
 dupfree_proteins_mem <- dupfree_proteins %>% collect()
-taxa                 <- dbpool %>% tbl('taxa')
+taxa                 <- con %>% tbl('taxa')
 taxa_mem             <- taxa %>% collect()
 
 # Collect late
@@ -45,35 +43,37 @@ system.time(
   }
 )
 
-# Collect after joins
-write("Collecting after joins, before filter and group_by", stderr())
-system.time(
-  {
-  d <- dupfree_proteins %>%
-    inner_join(accessions, by = 'accno') %>%
-    inner_join(hmm_profiles, by = 'profile') %>%
-    inner_join(taxa, by = 'taxon') %>%
-    collect() %>%
-    filter(db == 'refseq', pfamily == 'NrdJA', tdomain %in% c('Bacteria', 'Archaea', 'Eukaryota')) %>%
-    group_by(pclass, tdomain) %>% summarise(n = n()) %>% ungroup() %>%
-    spread(tdomain, n, fill = 0)
-  }
-)
+### # Collect after joins
+### # This is slow, commenting out
+### write("Collecting after joins, before filter and group_by", stderr())
+### system.time(
+###   {
+###   d <- dupfree_proteins %>%
+###     inner_join(accessions, by = 'accno') %>%
+###     inner_join(hmm_profiles, by = 'profile') %>%
+###     inner_join(taxa, by = 'taxon') %>%
+###     collect() %>%
+###     filter(db == 'refseq', pfamily == 'NrdJA', tdomain %in% c('Bacteria', 'Archaea', 'Eukaryota')) %>%
+###     group_by(pclass, tdomain) %>% summarise(n = n()) %>% ungroup() %>%
+###     spread(tdomain, n, fill = 0)
+###   }
+### )
 
-# Collect individual tables
-write("Collecting individual tables", stderr())
-system.time(
-  {
-  d <- dupfree_proteins %>% collect() %>%
-    inner_join(accessions %>% collect(), by = 'accno') %>%
-    inner_join(hmm_profiles %>% collect(), by = 'profile') %>%
-    inner_join(taxa %>% collect(), by = 'taxon') %>%
-    collect() %>%
-    filter(db == 'refseq', pfamily == 'NrdJA', tdomain %in% c('Bacteria', 'Archaea', 'Eukaryota')) %>%
-    group_by(pclass, tdomain) %>% summarise(n = n()) %>% ungroup() %>%
-    spread(tdomain, n, fill = 0)
-  }
-)
+### # Collect individual tables
+### # This is slow, commenting out
+### write("Collecting individual tables", stderr())
+### system.time(
+###   {
+###   d <- dupfree_proteins %>% collect() %>%
+###     inner_join(accessions %>% collect(), by = 'accno') %>%
+###     inner_join(hmm_profiles %>% collect(), by = 'profile') %>%
+###     inner_join(taxa %>% collect(), by = 'taxon') %>%
+###     collect() %>%
+###     filter(db == 'refseq', pfamily == 'NrdJA', tdomain %in% c('Bacteria', 'Archaea', 'Eukaryota')) %>%
+###     group_by(pclass, tdomain) %>% summarise(n = n()) %>% ungroup() %>%
+###     spread(tdomain, n, fill = 0)
+###   }
+### )
 
 # From in memory tables
 write("From in memory tables", stderr())
@@ -90,4 +90,32 @@ system.time(
   }
 )
 
-poolClose(dbpool)
+# From in memory tables
+write("From in memory tables, filter early", stderr())
+system.time(
+  {
+  d <- dupfree_proteins_mem %>%
+    inner_join(accessions_mem %>% filter(db == 'refseq'), by = 'accno') %>%
+    inner_join(hmm_profiles_mem %>% filter(pfamily == 'NrdJA'), by = 'profile') %>%
+    inner_join(taxa_mem %>% filter(tdomain %in% c('Bacteria', 'Archaea', 'Eukaryota')), by = 'taxon') %>%
+    group_by(pclass, tdomain) %>% summarise(n = n()) %>% ungroup() %>%
+    collect() %>%
+    spread(tdomain, n, fill = 0)
+  }
+)
+
+# From in memory tables
+write("From in memory tables, filter early, drive with accessions", stderr())
+system.time(
+  {
+  d <- accessions_mem %>% filter(db == 'refseq') %>%
+    inner_join(taxa_mem %>% filter(tdomain %in% c('Bacteria', 'Archaea', 'Eukaryota')), by = 'taxon') %>%
+    inner_join(dupfree_proteins_mem, by = 'accno') %>%
+    inner_join(hmm_profiles_mem %>% filter(pfamily == 'NrdJA'), by = 'profile') %>%
+    group_by(pclass, tdomain) %>% summarise(n = n()) %>% ungroup() %>%
+    collect() %>%
+    spread(tdomain, n, fill = 0)
+  }
+)
+
+con %>% DBI::dbDisconnect()
